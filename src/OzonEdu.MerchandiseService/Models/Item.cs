a0c23dea5961ad6877b237c3b58c9c15bd136a12
Александр Нейчев <alexnejchev73@gmail.com>
2021-10-26 15:01:45 +0400

    Add Model

---
+++ src/OzonEdu.MerchandiseService/Models/Item.cs
@@ -0,0 +1,9 @@
+namespace OzonEdu.MerchandiseService.Models
+{
+    public class Item
+    {
+        public long ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
