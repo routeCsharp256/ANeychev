3fad1ae42c1dcdbf026d83d7dbe9498c9f18011e
Александр Нейчев <alexnejchev73@gmail.com>
2021-10-29 16:48:45 +0400

    Add lunch swagger

--- src/OzonEdu.Infrastructure/Extensions/HostBuilderExtensions.cs
+++ src/OzonEdu.Infrastructure/Extensions/HostBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,13 +20,19 @@ namespace OzonEdu.Infrastructure.Extensions
                 services.AddSingleton<IStartupFilter, ReadyStartupFilter>();
                 services.AddSingleton<IStartupFilter, VersionStartupFilter>();
                 services.AddSingleton<IStartupFilter, RequestLoggingStartupFilter>();
+                services.AddSingleton<IStartupFilter, SwaggerStartupFilter>();
 
                 services.AddSwaggerGen(options =>
                 {
                     options.SwaggerDoc("v1",
                         new OpenApiInfo
-                            {Title = Assembly.GetEntryAssembly()?.GetName().Name ?? "no name", Version = "v1"});
+                            {Title = Assembly.GetEntryAssembly()?.GetName().Name, Version = "v1"});
                     options.CustomSchemaIds(x => x.FullName);
+                    var xmlFileName = Assembly.GetEntryAssembly()?.GetName().Name + ".xml";
+                    var xmlFilePath =
+                        Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location) ?? string.Empty,
+                            xmlFileName);
+                    options.IncludeXmlComments(xmlFilePath);
                 });
 
                 services.AddControllers(options => options.Filters.Add<GlobalExceptionFilter>());
--- src/OzonEdu.MerchandiseService/Controllers/V1/MerchandiseController.cs
+++ src/OzonEdu.MerchandiseService/Controllers/V1/MerchandiseController.cs
@@ -7,15 +7,28 @@ using OzonEdu.MerchandiseService.Services.Interfaces;
 
 namespace OzonEdu.MerchandiseService.Controllers.V1
 {
+    /// <summary>
+    /// Сервис учёта выдачи мерча сотрудникам
+    /// </summary>
     [ApiController]
-    [Route("v1/api/merchandises/{employeeId:long}")]
+    [Route("v1/merchandises/[controller]/[action]/{employeeId:long}")]
     [Produces("application/json")]
     public class MerchandiseController : ControllerBase
     {
         private readonly IMerchService _merchService;
 
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="merchService"></param>
         public MerchandiseController(IMerchService merchService) => _merchService = merchService;
 
+        /// <summary>
+        /// Запрос на выдачу мерча
+        /// </summary>
+        /// <param name="employeeId">идентификатор работника</param>
+        /// <param name="cancellationToken">токен отмены</param>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IActionResult> GiveOut(long employeeId, CancellationToken cancellationToken)
         {
@@ -23,6 +36,12 @@ namespace OzonEdu.MerchandiseService.Controllers.V1
             return Ok();
         }
 
+        /// <summary>
+        /// Возвращает информацию о выдаче мерча
+        /// </summary>
+        /// <param name="employeeId">идентификатор работника</param>
+        /// <param name="cancellationToken">токен отмены</param>
+        /// <returns></returns>
         [HttpGet]
         public async Task<ActionResult<List<MerchItemResponse>>> GetInfo(long employeeId,
             CancellationToken cancellationToken) =>
