d423e6c64453049c9b78f453ba879b1afd47e197
Александр Нейчев <alexnejchev73@gmail.com>
2021-11-01 15:12:46 +0400

    Add message for ReadyMiddleware

--- src/OzonEdu.Infrastructure/Middlewares/ReadyMiddleware.cs
+++ src/OzonEdu.Infrastructure/Middlewares/ReadyMiddleware.cs
@@ -9,6 +9,6 @@ namespace OzonEdu.Infrastructure.Middlewares
         {
         }
 
-        public async Task InvokeAsync(HttpContext context) => await context.Response.WriteAsync(string.Empty);
+        public async Task InvokeAsync(HttpContext context) => await context.Response.WriteAsync("Service is ready");
     }
 }
