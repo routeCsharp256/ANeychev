d06b7525439441999502de5ea27979bba68d522e
Александр Нейчев <alexnejchev73@gmail.com>
2021-11-05 15:27:44 +0400

    Add MerchandiseRequestRepository

---
+++ tests/OzonEdu.MerchandiseService.Domain.Tests/MerchandiseRequestRepository.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace OzonEdu.MerchandiseService.Domain.Tests
+{
+
+    public class MerchandiseRequestRepository
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
