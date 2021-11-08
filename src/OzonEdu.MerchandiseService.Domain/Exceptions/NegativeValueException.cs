efdeaffa019d4415ece82a9c5037ddbdeb5eee9e
Александр Нейчев <alexnejchev73@gmail.com>
2021-11-08 10:09:51 +0400

    Add Exceptions

---
+++ src/OzonEdu.MerchandiseService.Domain/Exceptions/NegativeValueException.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace OzonEdu.MerchandiseService.Domain.Exceptions
+{
+    public class NegativeValueException : Exception
+    {
+        public NegativeValueException(string message) : base(message)
+        {
+            
+        }
+        
+        public NegativeValueException(string message, Exception innerException) : base(message, innerException)
+        {
+            
+        }
+    }
+}
