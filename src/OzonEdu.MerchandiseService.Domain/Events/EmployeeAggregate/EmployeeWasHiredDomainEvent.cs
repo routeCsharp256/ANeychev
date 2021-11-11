efbdb269b29caf9e9d4185d47cb934c9bbbf4676
Александр Нейчев <alexnejchev73@gmail.com>
2021-11-11 17:50:51 +0400

    Implement EmployeeWasHiredDomainEvent

--- src/OzonEdu.MerchandiseService.Domain/Events/EmployeeAggregate/EmployeeWasHiredDomainEvent.cs
+++ src/OzonEdu.MerchandiseService.Domain/Events/EmployeeAggregate/EmployeeWasHiredDomainEvent.cs
@@ -4,5 +4,6 @@ namespace OzonEdu.MerchandiseService.Domain.Events.EmployeeAggregate
 {
     public class EmployeeWasHiredDomainEvent : INotification
     {
+        public long EmployeeId { get; set; }
     }
 }
--- src/OzonEdu.MerchandiseService.Infrastructure/DomainEvents/EmployeeAggregate/EmployeeWasHiredDomainEventHandler.cs
+++ src/OzonEdu.MerchandiseService.Infrastructure/DomainEvents/EmployeeAggregate/EmployeeWasHiredDomainEventHandler.cs
@@ -1,30 +1,33 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using CSharpCourse.Core.Lib.Enums;
 using MediatR;
-using OzonEdu.MerchandiseService.Domain.AggregationModels.EmployeeAggregate;
-using OzonEdu.MerchandiseService.Domain.AggregationModels.MerchPackAggregate;
 using OzonEdu.MerchandiseService.Domain.Events.EmployeeAggregate;
+using OzonEdu.MerchandiseService.Infrastructure.Services.Interfaces;
 
 namespace OzonEdu.MerchandiseService.Infrastructure.DomainEvents.EmployeeAggregate
 {
     public sealed class EmployeeWasHiredDomainEventHandler : INotificationHandler<EmployeeWasHiredDomainEvent>
     {
-        private readonly IEmployeeRepository _employeeRepository;
-        private readonly IMerchPackRepository _merchPackRepository;
+        private readonly IApplicationService _applicationService;
 
-        public EmployeeWasHiredDomainEventHandler(IEmployeeRepository employeeRepository,
-            IMerchPackRepository merchPackRepository)
-        {
-            _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
-            _merchPackRepository = merchPackRepository ?? throw new ArgumentNullException(nameof(merchPackRepository));
-        }
+        public EmployeeWasHiredDomainEventHandler(IApplicationService applicationService) =>
+            _applicationService = applicationService ?? throw new ArgumentNullException(nameof(applicationService));
 
         public async Task Handle(EmployeeWasHiredDomainEvent notification,
             CancellationToken cancellationToken = default)
         {
-            // TODO Проверить наличие заявок на WelcomePack, если заявка отсутствует, то создать её.
-            throw new System.NotImplementedException();
+            var employee = await _applicationService.GetEmployeeAsync(notification.EmployeeId, cancellationToken);
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+            var merchPack =
+                await _applicationService.GetMerchPackAsync((int) MerchType.WelcomePack,
+                    cancellationToken);
+            if (await _applicationService.CheckRepeatedMerchRequestAsync(employee, merchPack, cancellationToken))
+                return;
+            var merchRequest =
+                await _applicationService.CreateMerchRequestAsync(employee, merchPack, cancellationToken);
+            await _applicationService.TryGiveOutMerchRequestAsync(merchRequest, cancellationToken);
         }
     }
 }
