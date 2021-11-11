795619ff8e65607688196cc49241677659c89d4b
Александр Нейчев <alexnejchev73@gmail.com>
2021-11-11 17:50:36 +0400

    Implement EmployeeWasDismissedDomainEvent

--- src/OzonEdu.MerchandiseService.Domain/Events/EmployeeAggregate/EmployeeWasDismissedDomainEvent.cs
+++ src/OzonEdu.MerchandiseService.Domain/Events/EmployeeAggregate/EmployeeWasDismissedDomainEvent.cs
@@ -4,5 +4,6 @@ namespace OzonEdu.MerchandiseService.Domain.Events.EmployeeAggregate
 {
     public class EmployeeWasDismissedDomainEvent : INotification
     {
+        public long EmployeeId { get; set; }
     }
 }
--- src/OzonEdu.MerchandiseService.Infrastructure/DomainEvents/EmployeeAggregate/EmployeeWasDismissedDomainEventHandler.cs
+++ src/OzonEdu.MerchandiseService.Infrastructure/DomainEvents/EmployeeAggregate/EmployeeWasDismissedDomainEventHandler.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using OzonEdu.MerchandiseService.Domain.AggregationModels.EmployeeAggregate;
-using OzonEdu.MerchandiseService.Domain.AggregationModels.MerchPackAggregate;
+using OzonEdu.MerchandiseService.Domain.AggregationModels.MerchRequestAggregate;
 using OzonEdu.MerchandiseService.Domain.Events.EmployeeAggregate;
 
 namespace OzonEdu.MerchandiseService.Infrastructure.DomainEvents.EmployeeAggregate
@@ -11,20 +12,35 @@ namespace OzonEdu.MerchandiseService.Infrastructure.DomainEvents.EmployeeAggrega
     public sealed class EmployeeWasDismissedDomainEventHandler : INotificationHandler<EmployeeWasDismissedDomainEvent>
     {
         private readonly IEmployeeRepository _employeeRepository;
-        private readonly IMerchPackRepository _merchPackRepository;
+        private readonly IMerchRequestRepository _merchRequestRepository;
 
         public EmployeeWasDismissedDomainEventHandler(IEmployeeRepository employeeRepository,
-            IMerchPackRepository merchPackRepository)
+            IMerchRequestRepository merchRequestRepository)
         {
             _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
-            _merchPackRepository = merchPackRepository ?? throw new ArgumentNullException(nameof(merchPackRepository));
+            _merchRequestRepository =
+                merchRequestRepository ?? throw new ArgumentNullException(nameof(merchRequestRepository));
         }
 
         public async Task Handle(EmployeeWasDismissedDomainEvent notification,
             CancellationToken cancellationToken = default)
         {
-            // TODO Аннулировать все не завершенные заявки уволенного сотрудника
-            throw new System.NotImplementedException();
+            var employee = await _employeeRepository.FindByIdAsync(notification.EmployeeId, cancellationToken);
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+            var merchRequests = (await _merchRequestRepository.GetByEmployeeIdAsync(employee.Id, cancellationToken)).ToList()
+                .FindAll(x => Equals(x.Status, RequestStatus.InProgress));
+
+            if (merchRequests.Count > 0)
+            {
+                foreach (var merchRequest in merchRequests)
+                {
+                    merchRequest.Cancel();
+                    await _merchRequestRepository.UpdateAsync(merchRequest, cancellationToken);
+                }
+
+                await _merchRequestRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+            }
         }
     }
 }
