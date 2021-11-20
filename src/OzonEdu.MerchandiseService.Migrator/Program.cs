c82020400d5924782916da6b8b344fd34dc2db3d
Александр Нейчев <alexnejchev73@gmail.com>
2021-11-20 20:50:19 +0400

    Add MerchandiseService.Migrator

---
+++ src/OzonEdu.MerchandiseService.Migrator/Program.cs
@@ -0,0 +1,44 @@
+using System.IO;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace OzonEdu.MerchandiseService.Migrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration.GetSection("DatabaseConnectionOptions:ConnectionString").Get<string>();
+
+            var services = new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(
+                    rb => rb
+                        .AddPostgres()
+                        .WithGlobalConnectionString(connectionString)
+                        .ScanIn(typeof(Program).Assembly)
+                        .For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole());
+            
+            var serviceProvider = services.BuildServiceProvider(false);
+            
+            using (serviceProvider.CreateScope())
+            {
+                var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+                runner.ListMigrations();
+                runner.MigrateUp();
+                using var connection = new NpgsqlConnection(connectionString);
+                connection.Open();
+                connection.ReloadTypes();
+            }
+        }
+    }
+}
