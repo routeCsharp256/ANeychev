a8fe772fd6def3f651c5a7e06e1c948a276bbb35
Александр Нейчев <alexnejchev73@gmail.com>
2021-11-24 14:15:44 +0400

    Modify MerchRequestWorker

--- src/OzonEdu.MerchandiseService.Infrastructure/Workers/MerchRequestWorker.cs
+++ src/OzonEdu.MerchandiseService.Infrastructure/Workers/MerchRequestWorker.cs
@@ -2,7 +2,6 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using OzonEdu.MerchandiseService.Domain.AggregationModels.EmployeeAggregate;
 using OzonEdu.MerchandiseService.Domain.AggregationModels.MerchRequestAggregate;
 using OzonEdu.MerchandiseService.Domain.AggregationModels.ValueObjects;
 using OzonEdu.MerchandiseService.Domain.Contracts;
@@ -12,25 +11,20 @@ using OzonEdu.MerchandiseService.Infrastructure.Workers.Interfaces;
 
 namespace OzonEdu.MerchandiseService.Infrastructure.Workers
 {
-    public sealed class MerchRequestWorker : IMerchRequestWorker
+    public sealed class MerchRequestWorker : IMerchRequestWorker, IDisposable
     {
         private readonly IMerchRequestRepository _merchRequestRepository;
-        private readonly IEmployeeRepository _employeeRepository;
         private readonly IApplicationService _applicationService;
         private readonly IUnitOfWork _unitOfWork;
-        // ====
         private readonly IEmployeeHttpClient _employeeHttpClient;
 
         public MerchRequestWorker(IMerchRequestRepository merchRequestRepository,
-            IEmployeeRepository employeeRepository,
             IEmployeeHttpClient employeeHttpClient,
-            IApplicationService applicationService, 
+            IApplicationService applicationService,
             IUnitOfWork unitOfWork)
         {
             _merchRequestRepository =
                 merchRequestRepository ?? throw new ArgumentNullException(nameof(merchRequestRepository));
-
-            _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
             _employeeHttpClient = employeeHttpClient ?? throw new ArgumentNullException(nameof(employeeHttpClient));
             _applicationService = applicationService ?? throw new ArgumentNullException(nameof(applicationService));
             _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
@@ -38,28 +32,22 @@ namespace OzonEdu.MerchandiseService.Infrastructure.Workers
 
         public async Task AutoGenerateMerckRequests(CancellationToken cancellationToken = default)
         {
-            var externalEmployees = await _employeeHttpClient.GetAllAsync(cancellationToken) ??
-                                    throw new ArgumentException("External employee doesn't exist");
+            var employees = await _employeeHttpClient.GetAllAsync(cancellationToken) ??
+                            throw new ArgumentException("External employee doesn't exist");
 
-            foreach (var externalEmployee in externalEmployees)
+            foreach (var employee in employees)
             {
-                var employee =
-                    await _employeeRepository.FindByEmailAsync(Email.Create(externalEmployee.email), cancellationToken);
-                if (employee is null)
-                {
-                    var createEmployee = new Employee(new EmployeeFirstName(externalEmployee.firstName),
-                        new EmployeeMiddleName(externalEmployee.middleName),
-                        new EmployeeLastName(externalEmployee.lastName),
-                        new BirthDay(externalEmployee.birthDay),
-                        new HiringDate(externalEmployee.hiringDate),
-                        Email.Create(externalEmployee.email));
-                    await _unitOfWork.StartTransaction(cancellationToken);
-                    await _employeeRepository.CreateAsync(createEmployee, cancellationToken);
-                    await _unitOfWork.SaveChangesAsync(cancellationToken);
-                    employee = await _employeeRepository.FindByIdAsync(createEmployee.Id, cancellationToken);
-                }
-
-                employee.CheckWorkExperience(DateTime.Today);
+                var merchPackType = CheckWorkExperience(employee.hiringDate);
+                if (merchPackType is null) continue;
+                var merchPack = await _applicationService.GetMerchPackAsync(merchPackType.Id, cancellationToken);
+                
+                //TODO 1. Откуда брать почту ответственного менеджера при автоматической генерации заявок?
+                //TODO 2. Откуда брать размер сотрудника при автоматической генерации заявок?
+                
+                if (await _applicationService.IsNotRepeatedMerchRequestAsync(employee.id, merchPack, cancellationToken))
+                    await _applicationService.CreateMerchRequestAsync(employee.id, ClothingSize.XXL,
+                        Email.Create(employee.email), Email.Create("resposible_manager@mail.com"), merchPack,
+                        cancellationToken);
             }
         }
 
@@ -72,6 +60,29 @@ namespace OzonEdu.MerchandiseService.Infrastructure.Workers
 
             foreach (var actualMerchRequest in actualMerchRequests)
                 await _applicationService.TryGiveOutMerchRequestAsync(actualMerchRequest, cancellationToken);
+        }
+
+        //TODO добавить автоматическое назначение ответственного менеджера
+
+        private static MerchPackType? CheckWorkExperience(DateTimeOffset hiringDate)
+        {
+            var today = DateTimeOffset.UtcNow;
+            if (today.CompareTo(hiringDate) > 0) throw new ArgumentException(nameof(hiringDate));
+
+            var calcWorkExperience =
+                (hiringDate.Year * 12 + hiringDate.Month) - (today.Year * 12 + today.Month);
+            return calcWorkExperience switch
+            {
+                0 => MerchPackType.WelcomePack,
+                4 => MerchPackType.ProbationPeriodEndingPack,
+                61 => MerchPackType.VeteranPack,
+                _ => null
+            };
+        }
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
         }
     }
 }
