183492b2c59ea2682e1490ca9d8cfa4f40ee83d3
Александр Нейчев <alexnejchev73@gmail.com>
2021-11-24 14:02:02 +0400

    Modify MerchRequest events

--- src/OzonEdu.MerchandiseService.Domain/Events/MerchRequestAggregate/SupplyArrivedWithStockItemsDomainEvent.cs
+++ src/OzonEdu.MerchandiseService.Domain/Events/MerchRequestAggregate/SupplyArrivedWithStockItemsDomainEvent.cs
@@ -1,13 +1,12 @@
 ﻿using MediatR;
-using OzonEdu.MerchandiseService.Domain.AggregationModels.MerchPackAggregate;
 using OzonEdu.MerchandiseService.Domain.AggregationModels.ValueObjects;
 
-namespace OzonEdu.MerchandiseService.Domain.Events.MerchPackAggregate
+namespace OzonEdu.MerchandiseService.Domain.Events.MerchRequestAggregate
 {
     /// <summary>
     /// Пришла поставка с новыми товарами
     /// </summary>
-    public class SupplyArrivedWithStockItemsDomainEvent : INotification
+    public sealed class SupplyArrivedWithStockItemsDomainEvent : INotification
     {
         public Sku StockItemSku { get; }
         public Quantity Quantity { get; }
--- src/OzonEdu.MerchandiseService.Infrastructure/DomainEvents/MerchPackAggregate/SupplyArrivedWithStockItemsDomainEventHandler.cs
+++ src/OzonEdu.MerchandiseService.Infrastructure/DomainEvents/MerchPackAggregate/SupplyArrivedWithStockItemsDomainEventHandler.cs
@@ -2,7 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
-using OzonEdu.MerchandiseService.Domain.Events.MerchPackAggregate;
+using OzonEdu.MerchandiseService.Domain.Events.MerchRequestAggregate;
 using OzonEdu.MerchandiseService.Infrastructure.Workers.Interfaces;
 
 namespace OzonEdu.MerchandiseService.Infrastructure.DomainEvents.MerchPackAggregate
